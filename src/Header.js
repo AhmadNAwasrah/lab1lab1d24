90ae1549b3c135c2cceb8de9476096bdef360711
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-06-20 18:42:51 +0300

    first try

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+class Header extends React.Component()
+{
+render()
+{
+return(  <nav>
+    <h1>Welcome To Home Page</h1>
+    <ul>
+        <li>Home</li>
+        <li>Contact us</li>
+        <li>about us</li>
+    </ul>
+</nav>)
+
+
+}
+
+
+
+
+
+}
+export default Header;
