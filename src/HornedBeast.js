c0fefb5a5cb7d4a50743cc0d5b247ea19c5d369d
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-06-21 20:50:12 +0300

    lab 02 301d24

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
- class HornedBeast extends Component {
-    render() { 
-        console.log(this.props)
+class HornedBeast extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicks:this.props.clicks,
+        }
+    }
+    incresClick = () => {
+        this.setState({ clicks:this.state.clicks+1 })
+
+    }
+    render() {
+
         return (
             <div>
-                <h1>Hello </h1>
-                <img  height='300px' height='300px'  src={this.props.image_url} / >
-                <p></p>
+                 <h2> {this.props.title}</h2>
+                <img onClick={this.incresClick} height='300px' height='300px' src={this.props.image_url}  />
+                <p>{this.props.description} </p>
+                <span color='red' font='bold'>{this.state.clicks}</span>
             </div>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -1,45 +1,19 @@
 
 import React, { Component } from 'react';
 import HornedBeast from './HornedBeast';
+import DataB from './data.json'
+
 
 class Main extends Component {
     render() {
-        let data = [{
-            "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-            "title": "UniWhal",
-            "description": "A unicorn and a narwhal nuzzling their horns",
-            "keyword": "narwhal",
-            "horns": 1
-        }, {
-            "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-            "title": "Rhino Family",
-            "description": "Mother (or father) rhino with two babies",
-            "keyword": "rhino",
-            "horns": 2
         
-        }, {
-            "image_url": "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-            "title": "Unicorn Head",
-            "description": "Someone wearing a creepy unicorn head mask",
-            "keyword": "unicorn", 
-            "horns": 1
-        }];
         return (
-            <div> <HornedBeast image_url={data[0].image_url} title={data[0].title} description={data[0].description}
-            keyword={data[0].keyword} horns={data[0].horns} />
-            { <HornedBeast image_url={data[1].image_url} title={data[1].title} description={data[1].description}
-            keyword={data[1].keyword} horns={data[1].horns} /> }
-            <HornedBeast image_url={data[2].image_url} title={data[2].title} description={data[2].description}
-            keyword={data[2].keyword} horns={data[2].horns} /> 
+            <div>  {DataB.map((best,index)=>{
+              return  <HornedBeast clicks={0} image_url={best.image_url} title={best.title} description={best.description}
+           />})}
+             
 
-                {/* <h1>Welcome to HornedBeast</h1>
-          <img height='300px' height='300px' src='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-    "title": "UniWhal' alt='A unicorn and a narwhal nuzzling their horns' title='narwhal'/>
-    <img height='300px' height='300px' src='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80' 
-     alt='Mother (or father) rhino with two babies' title='rhino'/>
-    <img height='300px' height='300px' src='https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'
-    title='Unicorn Head' alt='Someone wearing a creepy unicorn head mask' title='unicorn'/> */}
-            </div>
+                 </div>
         )
 
     }
